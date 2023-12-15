24487aa4713f2352c36f13b9564f99dea0211a2e
Prajwal Pawar <114561900+PrajwalPawar789@users.noreply.github.com>
2023-12-16 04:27:29 +0530

    Add files via upload
    React-Redux app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Users from './Users';
+ 
+function App() {
+  return (
+    <div className="App">
+      <h1>App Component</h1>
+      
+      <Users name={"Manlitics"} age={"4years"}/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Users.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Users = (props) => {
+  return (
+    <div>
+    Users
+    <h1>
+    {props.name}
+    
+    {props.age}
+    </h1>
+    
+    </div>
+  )
+}
+
+export default Users
